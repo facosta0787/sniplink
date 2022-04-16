6ce55aa3a79073897ff26fe32fe37b1c636b1655
Felipe Acosta <facosta0787@gmail.com>
2022-04-15 20:44:51 -0500

    SNL-1 | add gitlab ci pipeline (#1)
    * SNL-1 | Added gitlab ci configuration
    
    * SNL-1 | Updated external pull request iid

--- src/config/scripts/review.deployment.sh
+++ src/config/scripts/review.deployment.sh
@@ -2,8 +2,8 @@
 
 VERCEL_ORG_ID=$VERCEL_ORG_ID \
 VERCEL_PROJECT_ID=$VERCEL_PROJECT_ID \
+TOKEN=$VERCEL_TOKEN \
 COMMIT_SHA=$CI_COMMIT_SHA \
-COMMIT_SHORT_SHA=$CI_COMMIT_SHORT_SHA \
-ENV_NAME=$REVIEW_ENV_NAME \
+URL=$REVIEW_URL \
 VERCEL_DEPLOYMENT=$(node_modules/.bin/vercel -e NODE_ENV=staging --meta commit=$COMMIT_SHA --no-clipboard --token=$VERCEL_TOKEN) &&
-node_modules/.bin/vercel alias $VERCEL_DEPLOYMENT $REVIEW_ENV_NAME-$COMMIT_SHORT_SHA.sniplink.tk --token=$VERCEL_TOKEN
+node_modules/.bin/vercel alias $VERCEL_DEPLOYMENT $URL --token=$VERCEL_TOKEN
--- src/config/scripts/review.destroy.sh
+++ src/config/scripts/review.destroy.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 TOKEN=$VERCEL_TOKEN
-REVIEW_URL=$VERCEL_REVIEW_URL
+URL=$REVIEW_URL
 
-VERCEL_DEPLOYMENT_ID=$(curl "https://api.vercel.com/v2/now/aliases/$REVIEW_URL" \
+VERCEL_DEPLOYMENT_ID=$(curl -k "https://api.vercel.com/v2/now/aliases/$URL" \
   -H "Accept: application/json" \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $TOKEN" \
-| config/scripts/bin/jq-linux ".deploymentId" \ # Using jq  for taking the deploymentId value
-| sed 's/"//g') # Removing double quotes
+| config/scripts/bin/jq-linux ".deploymentId" \
+| sed 's/"//g')
 
 if [ -z ${VERCEL_DEPLOYMENT_ID+x} ] || [ -z ${VERCEL_DEPLOYMENT_ID} ]; then
   echo "Deployment not found"
   exit 1
 else
   echo "Removing deployment '$VERCEL_DEPLOYMENT_ID'"
-  curl -X DELETE "https://api.vercel.com/v11/now/deployments/$VERCEL_DEPLOYMENT_ID" -H "Authorization: Bearer $TOKEN"
+  curl -k -X DELETE "https://api.vercel.com/v11/now/deployments/$VERCEL_DEPLOYMENT_ID" -H "Authorization: Bearer $TOKEN"
   exit 0
 fi
