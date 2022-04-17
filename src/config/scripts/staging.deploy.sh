4f77d838d17f771689323e918bdf329de8d150cc
Felipe Acosta <facosta0787@gmail.com>
2022-04-16 19:33:52 -0500

    MASTER | Added deploy stage job

--- src/config/scripts/staging.deploy.sh
+++ src/config/scripts/staging.deploy.sh
@@ -3,4 +3,4 @@
 VERCEL_ORG_ID=$VERCEL_ORG_ID \
 VERCEL_PROJECT_ID=$VERCEL_PROJECT_ID \
 VERCEL_DEPLOYMENT=$(node_modules/.bin/vercel -e NODE_ENV=staging --meta commit=$CI_COMMIT_SHA --no-clipboard --token=$VERCEL_TOKEN) &&
-node_modules/.bin/vercel alias $VERCEL_DEPLOYMENT stage.gitprofile.ga --token=$VERCEL_TOKEN
+node_modules/.bin/vercel alias $VERCEL_DEPLOYMENT stage.sniplink.tk --token=$VERCEL_TOKEN
