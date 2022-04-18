08e6f4a208a68516f5258d8664ae0b15dcda45eb
Felipe Acosta <facosta0787@gmail.com>
2022-04-18 13:22:13 -0500

    SNL-1 | deploy perview app (#2)
    * SNL-1 | deploy perview app
    
    * SNL-1 | Fixed typo on workflow file
    
    * SNL-1 | Fixed commands on review.deployment.sh file
    
    * SNL-1 | Added comment creation action
    
    * SNL-1 | Updated action that adds the comment

--- src/config/scripts/review.deployment.sh
+++ src/config/scripts/review.deployment.sh
@@ -5,5 +5,5 @@ VERCEL_PROJECT_ID=$VERCEL_PROJECT_ID \
 TOKEN=$VERCEL_TOKEN \
 COMMIT_SHA=$CI_COMMIT_SHA \
 URL=$REVIEW_URL \
-VERCEL_DEPLOYMENT=$(node_modules/.bin/vercel -e NODE_ENV=staging --meta commit=$COMMIT_SHA --no-clipboard --token=$VERCEL_TOKEN) &&
-node_modules/.bin/vercel alias $VERCEL_DEPLOYMENT $URL --token=$VERCEL_TOKEN
+VERCEL_DEPLOYMENT=$(npx --no-install vercel -e NODE_ENV=staging --meta commit=$COMMIT_SHA --no-clipboard --token=$VERCEL_TOKEN) &&
+npx --no-install vercel alias $VERCEL_DEPLOYMENT $URL --token=$VERCEL_TOKEN
