fc33788cbc7156e7ca822313cedd39ef1837ae93
Felipe Acosta <facosta0787@gmail.com>
2022-04-19 09:57:12 -0500

    SNL-6 | Added stage manually deployment

--- src/config/scripts/staging.deploy.sh
+++ src/config/scripts/staging.deploy.sh
@@ -2,5 +2,6 @@
 
 VERCEL_ORG_ID=$VERCEL_ORG_ID \
 VERCEL_PROJECT_ID=$VERCEL_PROJECT_ID \
+URL=$STAGE_URL \
 VERCEL_DEPLOYMENT=$(npx vercel -e NODE_ENV=staging --meta commit=$CI_COMMIT_SHA --no-clipboard --token=$VERCEL_TOKEN) &&
-npx vercel alias $VERCEL_DEPLOYMENT stage.sniplink.tk --token=$VERCEL_TOKEN
+npx vercel alias $VERCEL_DEPLOYMENT $URL --token=$VERCEL_TOKEN
