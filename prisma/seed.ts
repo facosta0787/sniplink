33694733081ba09675ca053180fc95a1e532a6bc
Felipe Acosta <facosta0787@gmail.com>
2022-12-27 16:26:31 -0500

    feat(SNL-9): Data migrated and added seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,451 @@
+import prisma from '../src/lib/db';
+
+const data = {
+  records: [
+    {
+      id: 'rec02nzo6zi49s1TU',
+      createdTime: '2022-11-24T14:37:20.000Z',
+      fields: {
+        alias: 'amazon-tablet-stand',
+        uid: 'ZXu3XC4J',
+        link: 'https://www.amazon.com/-/es/Soporte-ajustable-para-tablet-Kogoda/dp/B0B8SBNZJS/ref=sr_1_44?__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=3DFROI8RDOO1B&keywords=Tablet+stand&qid=1669299797&sprefix=table+stand%2Caps%2C279&sr=8-44',
+        Created: '2022-11-24T14:37:20.000Z',
+        'Last Modified': '2022-11-24T14:37:20.000Z',
+      },
+    },
+    {
+      id: 'rec1r50aa1I0rZUIG',
+      createdTime: '2022-10-26T16:04:41.000Z',
+      fields: {
+        alias: 'soydalto-javascript-junior',
+        uid: 'YQ1WsfMQ',
+        link: 'https://www.youtube.com/watch?v=z95mZVUcJ-E',
+        Created: '2022-10-26T16:04:41.000Z',
+        'Last Modified': '2022-10-26T16:04:41.000Z',
+      },
+    },
+    {
+      id: 'rec2U1OlFlClSyqXm',
+      createdTime: '2022-05-03T14:50:59.000Z',
+      fields: {
+        alias: 'chicago-activities',
+        uid: '1jNIHOUG',
+        link: 'https://elfin-pullover-35b.notion.site/Chicago-ee2694c81a2c4762a7617b197451d024',
+        Created: '2022-05-03T14:50:59.000Z',
+        'Last Modified': '2022-05-03T14:50:59.000Z',
+      },
+    },
+    {
+      id: 'rec4baNneRNvwBGFG',
+      createdTime: '2022-12-23T06:25:19.000Z',
+      fields: {
+        alias: 'tiktok-programador-nea',
+        uid: 'l6nbBm6o',
+        link: 'https://www.tiktok.com/@programadornea/video/7171440127577771269?is_copy_url=1&is_from_webapp=v1&lang=es',
+        Created: '2022-12-23T06:25:19.000Z',
+        'Last Modified': '2022-12-23T06:25:19.000Z',
+      },
+    },
+    {
+      id: 'rec7xIHN8MPVildhn',
+      createdTime: '2022-04-09T17:49:30.000Z',
+      fields: {
+        alias: 'mapaplus-europa-estilo-mp3',
+        uid: 'VIFUaMT5',
+        link: 'https://www.mapaplus.com/product?productCode=IT22T-EST3MF&fareCode=NORMAL&packageCode=IT22T-EST3MF&stayCode=20N&beginDate=21/04/2022',
+        Created: '2022-04-09T17:49:30.000Z',
+        'Last Modified': '2022-04-18T22:26:11.000Z',
+      },
+    },
+    {
+      id: 'rec8esydPdAjpxtKr',
+      createdTime: '2022-04-18T22:05:16.000Z',
+      fields: {
+        alias: 'europamundo-19dias-pdf',
+        uid: 'tWkaBB1q',
+        link: 'https://www.europamundo-online.com/documentos/pdf/dolares/2022/610.pdf',
+        Created: '2022-04-18T22:05:16.000Z',
+        'Last Modified': '2022-04-18T22:05:16.000Z',
+      },
+    },
+    {
+      id: 'rec9IxE1NLYZXA8Ly',
+      createdTime: '2021-12-17T19:13:54.000Z',
+      fields: {
+        uid: 'dSusnOva',
+        link: 'https://www.google.com/maps/@21.1605558,-86.8292392,3a,75y,110.74h,104.33t/data=!3m7!1e1!3m5!1skKMnpebv83_JidWJxjlhkQ!2e0!6shttps:%2F%2Fstreetviewpixels-pa.googleapis.com%2Fv1%2Fthumbnail%3Fpanoid%3DkKMnpebv83_JidWJxjlhkQ%26cb_client%3Dmaps_sv.tactile.gps%26w%3D203%26h%3D100%26yaw%3D82.275345%26pitch%3D0%26thumbfov%3D100!7i16384!8i8192',
+        Created: '2021-12-17T19:13:54.000Z',
+        'Last Modified': '2021-12-17T19:13:54.000Z',
+      },
+    },
+    {
+      id: 'rec9xCR2R3IxoFbDk',
+      createdTime: '2021-12-14T18:41:03.000Z',
+      fields: {
+        uid: 'aVqBXs4R',
+        link: 'https://elfin-pullover-35b.notion.site/Mexico-b05e104e8bd3478c81f42cde3bc79c21',
+        Created: '2021-12-14T18:41:03.000Z',
+        'Last Modified': '2021-12-14T18:41:03.000Z',
+      },
+    },
+    {
+      id: 'recDPteWow0z4j1XR',
+      createdTime: '2021-12-14T18:15:29.000Z',
+      fields: {
+        uid: 'ffGHStMO',
+        link: 'https://www.airbnb.com.co/rooms/40199866?adults=2&location=Zona%20Hotelera%2C%20Canc%C3%BAn%2C%20Q.R.%2C%20M%C3%A9xico&check_in=2022-02-08&check_out=2022-02-12&federated_search_id=ccc80638-6872-4025-bcba-7b68b2c5ec70&source_impression_id=p3_1639505661_A3sO4Je2gxxoem13&guests=1',
+        Created: '2021-12-14T18:15:29.000Z',
+        'Last Modified': '2021-12-14T18:15:29.000Z',
+      },
+    },
+    {
+      id: 'recFQS8WBZj7aWQmy',
+      createdTime: '2021-12-14T18:23:13.000Z',
+      fields: {
+        uid: 'rRPjGCNH',
+        link: 'https://www.airbnb.com.co/rooms/33553202/photos?adults=2&check_in=2022-02-08&check_out=2022-02-12&federated_search_id=8e6bbf56-c487-42a6-aece-67b6b864ba70&source_impression_id=p3_1639506021_L0B3k1rbVn4uyg8b&guests=1',
+        Created: '2021-12-14T18:23:13.000Z',
+        'Last Modified': '2021-12-14T18:23:13.000Z',
+      },
+    },
+    {
+      id: 'recGGTleDsRlgmNBQ',
+      createdTime: '2022-07-06T16:39:29.000Z',
+      fields: {
+        alias: 'xkweb-gastos-viaje-javascript',
+        uid: 'MLncaEMk',
+        link: 'https://www.youtube.com/watch?v=-fzu8BZe-Jg',
+        Created: '2022-07-06T16:39:29.000Z',
+        'Last Modified': '2022-07-06T16:39:29.000Z',
+      },
+    },
+    {
+      id: 'recGweEVKgOm9cFpP',
+      createdTime: '2021-12-14T18:39:20.000Z',
+      fields: {
+        uid: 'DeZUxcPD',
+        link: 'https://www.airbnb.com.co/rooms/4645376?adults=2&check_in=2022-02-08&check_out=2022-02-12&federated_search_id=ec50bc58-fcb7-476f-9f77-611c1f1d0674&source_impression_id=p3_1639507010_LafHFhvKNwsfWBkP&guests=1',
+        Created: '2021-12-14T18:39:20.000Z',
+        'Last Modified': '2021-12-14T18:39:20.000Z',
+      },
+    },
+    {
+      id: 'recJyOoRFHnMdI5qI',
+      createdTime: '2021-12-14T23:05:18.000Z',
+      fields: {
+        uid: 'r13MlIEQ',
+        link: 'https://www.google.com/maps/place/Lago+Tasimay+11,+Amp+Torre+Blanca,+Miguel+Hidalgo,+11289+Ciudad+de+M%C3%A9xico,+CDMX,+M%C3%A9xico/@19.4395017,-99.2048029,13z/data=!4m5!3m4!1s0x85d20272f600c92f:0x1f739f0dc5da9970!8m2!3d19.4565725!4d-99.1992293',
+        Created: '2021-12-14T23:05:18.000Z',
+        'Last Modified': '2021-12-14T23:05:18.000Z',
+      },
+    },
+    {
+      id: 'recKa1jcPjt5gbfmk',
+      createdTime: '2022-12-23T06:40:53.000Z',
+      fields: {
+        alias: 'amor-en-patines',
+        uid: 'uJjViNah',
+        link: 'https://www.tiktok.com/@aidacortesusa/video/7159559154053549317?is_copy_url=1&is_from_webapp=v1',
+        Created: '2022-12-23T06:40:53.000Z',
+        'Last Modified': '2022-12-23T06:40:53.000Z',
+      },
+    },
+    {
+      id: 'recNDeFF7H7HrArA2',
+      createdTime: '2021-12-15T01:11:31.000Z',
+      fields: {
+        uid: 'MVO3wXSf',
+        link: 'https://reservations.easy-rez.com/v2/hotel-kavia-plus/es/availability?pk_vid=bee87ff953b0abe516395303662c8f4d',
+        Created: '2021-12-15T01:11:31.000Z',
+        'Last Modified': '2021-12-15T01:11:31.000Z',
+      },
+    },
+    {
+      id: 'recOTqsdEc7cIjUUm',
+      createdTime: '2022-04-13T05:11:05.000Z',
+      fields: {
+        alias: 'backgrounds-desings',
+        uid: '8J7S1LhB',
+        link: 'https://haikei.app/',
+        Created: '2022-04-13T05:11:05.000Z',
+        'Last Modified': '2022-04-13T05:11:05.000Z',
+      },
+    },
+    {
+      id: 'recOXJ0clMUZcB555',
+      createdTime: '2022-06-06T13:50:23.000Z',
+      fields: {
+        alias: 'youtube-carrito-compras-practico',
+        uid: 'h8BYOnD0',
+        link: 'https://www.youtube.com/watch?v=JL7Wo-ASah4',
+        Created: '2022-06-06T13:50:23.000Z',
+        'Last Modified': '2022-06-06T13:50:23.000Z',
+      },
+    },
+    {
+      id: 'recPL1glojRSPFRi8',
+      createdTime: '2022-12-09T17:21:04.000Z',
+      fields: {
+        alias: 'aliexpress-minipc-r86s',
+        uid: 'oO3svzIO',
+        link: 'https://www.aliexpress.com/item/1005004182425248.html?spm=a2g0o.productlist.0.0.11bf6b4aUU6B70&algo_pvid=de453ead-1455-4689-8626-2dd0d479bc03&algo_exp_id=de453ead-1455-4689-8626-2dd0d479bc03-0&pdp_ext_f=%7B%22sku_id%22%3A%2212000030055747258%22%7D&pdp_npi=2%40dis%21USD%21315.99%21315.99%21%21%21%21%21%40210318bb16706063294078123eb411%2112000030055747258%21sea&curPageLogUid=BqBaxDfhpgC7',
+        Created: '2022-12-09T17:21:04.000Z',
+        'Last Modified': '2022-12-09T17:21:04.000Z',
+      },
+    },
+    {
+      id: 'recPSxIrxPTGRzxw2',
+      createdTime: '2022-04-13T04:43:32.000Z',
+      fields: {
+        alias: 'airtable-filterbytformula-multiple',
+        uid: 'z6JJMVdt',
+        link: 'https://community.airtable.com/t/filterbyformula-multiple-select/2075/2',
+        Created: '2022-04-13T04:43:32.000Z',
+        'Last Modified': '2022-04-13T04:43:32.000Z',
+      },
+    },
+    {
+      id: 'recRJ9tlghpFEbALA',
+      createdTime: '2022-07-06T16:44:44.000Z',
+      fields: {
+        alias: 'codificandolo-creando-reloj',
+        uid: 'pD4sKzjO',
+        link: 'https://www.youtube.com/watch?v=GtqHWm9qdno',
+        Created: '2022-07-06T16:44:44.000Z',
+        'Last Modified': '2022-07-06T16:44:44.000Z',
+      },
+    },
+    {
+      id: 'recSsuIcEGqPkMdGo',
+      createdTime: '2022-04-18T22:28:24.000Z',
+      fields: {
+        alias: 'github-todo-list',
+        uid: 'abOFU3dm',
+        link: 'https://facosta0787.github.io/todo-list/',
+        Created: '2022-04-18T22:28:24.000Z',
+        'Last Modified': '2022-04-18T22:28:24.000Z',
+      },
+    },
+    {
+      id: 'recUUtnQ5NHw91s62',
+      createdTime: '2022-07-05T15:02:06.000Z',
+      fields: {
+        alias: 'prisma-course-nexus-schema',
+        uid: 'ycMcNRIQ',
+        link: 'https://www.prisma.io/blog/fullstack-nextjs-graphql-prisma-oklidw1rhw',
+        Created: '2022-07-05T15:02:06.000Z',
+        'Last Modified': '2022-07-05T15:02:06.000Z',
+      },
+    },
+    {
+      id: 'recb02mLjbotlJikS',
+      createdTime: '2021-12-15T01:01:53.000Z',
+      fields: {
+        uid: 'ijrE2JhR',
+        link: 'https://www.flamingocancun.com/es/bookcore/availability/rooms/flamingocancun/?occupancies=%255B%257B%2522ages%2522%253A%2520%2522%2522%252C%2520%2522adults%2522%253A%25202%252C%2520%2522children%2522%253A%25200%257D%255D&priority_room=estandarmar&rrc=1&adults=2&occp=1',
+        Created: '2021-12-15T01:01:53.000Z',
+        'Last Modified': '2021-12-15T01:01:53.000Z',
+      },
+    },
+    {
+      id: 'recdaBSINv4uQbbfZ',
+      createdTime: '2022-07-01T19:56:02.000Z',
+      fields: {
+        alias: 'jonmircha-curso-javascript',
+        uid: 'lrwKyfYW',
+        link: 'https://www.youtube.com/watch?v=2SetvwBV-SU&list=PLvq-jIkSeTUZ6QgYYO3MwG9EMqC-KoLXA&index=1',
+        Created: '2022-07-01T19:56:02.000Z',
+        'Last Modified': '2022-07-01T19:56:02.000Z',
+      },
+    },
+    {
+      id: 'rece1GiVcERl7qNbN',
+      createdTime: '2022-06-06T15:32:40.000Z',
+      fields: {
+        alias: 'leonidas-social-media-dashboard',
+        uid: 'om0Umc3T',
+        link: 'https://www.youtube.com/watch?v=ScSHSI7TzRw',
+        Created: '2022-06-06T15:32:40.000Z',
+        'Last Modified': '2022-06-06T15:32:40.000Z',
+      },
+    },
+    {
+      id: 'rececHNiMrsEsFl1a',
+      createdTime: '2022-06-06T20:15:04.000Z',
+      fields: {
+        alias: 'leonidas-rick-and-morty',
+        uid: 'QRqhtiSV',
+        link: 'https://www.youtube.com/watch?v=5d22M9D31os&t=308s',
+        Created: '2022-06-06T20:15:04.000Z',
+        'Last Modified': '2022-06-06T20:15:04.000Z',
+      },
+    },
+    {
+      id: 'recf50z1VFuVCBzwy',
+      createdTime: '2021-12-16T21:40:35.000Z',
+      fields: {
+        uid: 'sCkAe6q3',
+        link: 'https://www.google.com/search?q=nextjs&oq=nextjs&aqs=edge..69i57j0i10i512j0i512j0i10i512l5j69i59.1530j0j1&sourceid=chrome&ie=UTF-8',
+        Created: '2021-12-16T21:40:35.000Z',
+        'Last Modified': '2021-12-16T21:40:35.000Z',
+      },
+    },
+    {
+      id: 'recfFKareBTvLyCSI',
+      createdTime: '2022-06-24T20:08:54.000Z',
+      fields: {
+        alias: 'fazt-code-react-fundamentos',
+        uid: 'I4eEegsq',
+        link: 'https://www.youtube.com/watch?v=sjrK6RA65eQ',
+        Created: '2022-06-24T20:08:54.000Z',
+        'Last Modified': '2022-06-24T20:08:54.000Z',
+      },
+    },
+    {
+      id: 'rechSggjFM8VnLY0F',
+      createdTime: '2022-12-23T21:19:36.000Z',
+      fields: {
+        alias: 'gdocs-finances',
+        uid: 'vVVy1Oyu',
+        link: 'https://docs.google.com/spreadsheets/d/15JzDWDzXT7tcAjwFkTq_5i_PuQrQ_habjXK6lrxJcmc/edit#gid=977012694',
+        Created: '2022-12-23T21:19:36.000Z',
+        'Last Modified': '2022-12-23T21:19:36.000Z',
+      },
+    },
+    {
+      id: 'reciK7Y8rRnWSOdMj',
+      createdTime: '2022-05-24T18:12:41.000Z',
+      fields: {
+        alias: 'amazon-watch-7-protector',
+        uid: '15avAKy5',
+        link: 'https://www.amazon.com/-/es/2%E3%80%91Protector-herramienta-alineaci%C3%B3n-antiara%C3%B1azos-resistente/dp/B09W22BJLC/ref=sr_1_5?keywords=protector%2Bwatch%2B7%2B45%2Bmm&qid=1653415917&sprefix=protector%2Bw%2Caps%2C145&sr=8-5&th=1',
+        Created: '2022-05-24T18:12:41.000Z',
+        'Last Modified': '2022-05-24T18:12:41.000Z',
+      },
+    },
+    {
+      id: 'recj3jLjFt6m7G4h0',
+      createdTime: '2022-04-22T16:18:50.000Z',
+      fields: {
+        alias: 'coding-fonts',
+        uid: 'Yb2Won6h',
+        link: 'https://drive.google.com/drive/folders/1Kv-AWpx0aanvyHRJRFSMBvTohEsR6mG-?usp=sharing',
+        Created: '2022-04-22T16:18:50.000Z',
+        'Last Modified': '2022-04-22T16:18:50.000Z',
+      },
+    },
+    {
+      id: 'recjXu1SOh4EcZmxR',
+      createdTime: '2022-06-07T12:37:30.000Z',
+      fields: {
+        alias: 'faztcode-javascript-fullstack',
+        uid: 'NoRShLM7',
+        link: 'https://www.youtube.com/playlist?list=PLo5lAe9kQrwq7n_REwpZdfggPCBW2ggnh',
+        Created: '2022-06-07T12:37:30.000Z',
+        'Last Modified': '2022-06-07T12:37:30.000Z',
+      },
+    },
+    {
+      id: 'reckUQUnUOJ6fTb9v',
+      createdTime: '2022-12-23T21:21:11.000Z',
+      fields: {
+        alias: 'prisma-client-reference',
+        uid: 'H4Ozfhzg',
+        link: 'https://www.prisma.io/docs/reference/api-reference/prisma-client-reference',
+        Created: '2022-12-23T21:21:11.000Z',
+        'Last Modified': '2022-12-23T21:21:11.000Z',
+      },
+    },
+    {
+      id: 'recltR1pqSWxznqsD',
+      createdTime: '2022-04-09T17:51:21.000Z',
+      fields: {
+        alias: 'mapaplus-europa-estilo-mp3-pdf',
+        uid: 'dXsieigH',
+        link: 'https://almacen.mapaplus.com/web/2021/Contenido/PDF/PDF_INDIVIDUALES/102_Europa_Estilo_MP3_2021.pdf',
+        Created: '2022-04-09T17:51:21.000Z',
+        'Last Modified': '2022-04-13T04:37:53.000Z',
+      },
+    },
+    {
+      id: 'recltan4kw3KnQD4i',
+      createdTime: '2022-06-24T20:24:06.000Z',
+      fields: {
+        alias: 'fazt-code-fetch-filter',
+        uid: 'N9O7EkPd',
+        link: 'https://www.youtube.com/watch?v=DgunJ2jMZwM',
+        Created: '2022-06-24T20:24:06.000Z',
+        'Last Modified': '2022-06-24T20:24:06.000Z',
+      },
+    },
+    {
+      id: 'recn935p04Ha3L08q',
+      createdTime: '2022-06-06T16:15:46.000Z',
+      fields: {
+        alias: 'leonidas-landing-bank',
+        uid: 'l6WSiVfU',
+        link: 'https://www.youtube.com/watch?v=O_KOi4t3GdM',
+        Created: '2022-06-06T16:15:46.000Z',
+        'Last Modified': '2022-06-06T16:15:46.000Z',
+      },
+    },
+    {
+      id: 'recuCqv47uguFqjJb',
+      createdTime: '2021-12-14T18:11:08.000Z',
+      fields: {
+        uid: 'z1YQepV0',
+        link: 'https://www.airbnb.com.co/rooms/47543689?adults=2&check_in=2022-02-08&check_out=2022-02-12&federated_search_id=ccc80638-6872-4025-bcba-7b68b2c5ec70&source_impression_id=p3_1639504564_s1CADMLc1ZXnqM%2Bf&guests=1',
+        Created: '2021-12-14T18:11:08.000Z',
+        'Last Modified': '2021-12-14T18:11:08.000Z',
+      },
+    },
+    {
+      id: 'recuRO15mxxqENmI5',
+      createdTime: '2022-07-08T18:01:19.000Z',
+      fields: {
+        alias: 'falconmasters-curso-es6',
+        uid: '17vwc5hQ',
+        link: 'https://www.youtube.com/watch?v=LWiMWZ9-cdw&list=PLhSj3UTs2_yX_ct0OfHrmMwKL8wpz-N2j',
+        Created: '2022-07-08T18:01:19.000Z',
+        'Last Modified': '2022-07-08T18:01:19.000Z',
+      },
+    },
+    {
+      id: 'recvThu0ZP4RUAJYL',
+      createdTime: '2021-12-14T20:08:34.000Z',
+      fields: {
+        uid: 'qTlHcsAt',
+        link: 'https://www.airbnb.com.co/rooms/51070004?adults=2&location=Zona%20Hotelera%2C%20Canc%C3%BAn%2C%20Q.R.%2C%20M%C3%A9xico&check_in=2022-02-08&check_out=2022-02-12&federated_search_id=8a6bf566-8ce8-4976-a17e-f2a5b044c4d7&source_impression_id=p3_1639512445_sC9YxLvtuoHZJTcI&guests=1',
+        Created: '2021-12-14T20:08:34.000Z',
+        'Last Modified': '2021-12-14T20:08:34.000Z',
+      },
+    },
+    {
+      id: 'recvzcOb6hryLvel0',
+      createdTime: '2022-07-05T15:09:18.000Z',
+      fields: {
+        alias: 'nrwl-nx-course',
+        uid: 'c5rpbPVz',
+        link: 'https://www.youtube.com/watch?v=2mYLe9Kp9VM&list=PLakNactNC1dH38AfqmwabvOszDmKriGco',
+        Created: '2022-07-05T15:09:18.000Z',
+        'Last Modified': '2022-07-05T15:09:18.000Z',
+      },
+    },
+  ],
+};
+
+async function seed() {
+  const mapAirtableToPrisma = data.records.map(({ fields }) => ({
+    hash: fields.uid,
+    link: fields.link,
+    alias: fields.alias || null,
+  }));
+
+  const result = await prisma.conn.link.createMany({
+    data: mapAirtableToPrisma,
+  });
+
+  console.log(result);
+}
+
+seed();
