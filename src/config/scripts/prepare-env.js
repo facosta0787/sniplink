f78ba6c73fa96c357f0878e4856b735cd8106708
Felipe Acosta <facosta0787@gmail.com>
2022-12-30 12:54:26 -0500

    feat(SNL-8): Testing preview deploy

--- src/config/scripts/prepare-env.js
+++ src/config/scripts/prepare-env.js
@@ -1,44 +1,75 @@
 const fetch = require('node-fetch/lib/index');
+const Caprover = require('../../lib/caprover-api');
 
-const SERVER_URL = process.env.CAPROVER_URL;
-const SERVER_NAME = process.env.CAPROVER_SERVER;
+const SERVER_URL = process.env.CAPROVER_SERVER_URL;
 const SERVER_KEY = process.env.CAPROVER_KEY;
+const APP_NAME = process.env.CAPROVER_APP_NAME;
+const DB_URL = process.env.CAPROVER_DB_URL;
+
+const DEFAULT_APP_CONFIG = {
+  appName: APP_NAME,
+  appDeployTokenConfig: {
+    enabled: true,
+  },
+  instanceCount: 1,
+  captainDefinitionRelativeFilePath: './captain-definition',
+  containerHttpPort: 3000,
+  notExposeAsWebApp: false,
+  forceSsl: true,
+  websocketSupport: false,
+  volumes: [],
+  ports: [],
+  description: '',
+  envVars: [
+    {
+      key: 'LINK_DOMAIN',
+      value: `https://${APP_NAME}.mdecloud.tk`,
+    },
+    {
+      key: 'DATABASE_URL',
+      value: DB_URL,
+    },
+  ],
+};
 
 async function main() {
   try {
-    const loginEndPoint = SERVER_URL + '/api/v2/login';
-    const registerAppEndPoint =
-      SERVER_URL + '/api/v2/user/apps/appDefinitions/register';
+    const caprover = await Caprover.init(SERVER_URL, SERVER_KEY);
+    const {
+      data: { appDefinitions },
+    } = await caprover.apps();
+    const app = appDefinitions.find((appDef) => appDef.appName === APP_NAME);
 
-    const resLogin = await fetch(loginEndPoint, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'x-namespace': 'captain',
-      },
-      method: 'POST',
-      body: JSON.stringify({ password: SERVER_KEY }),
-    });
+    if (!Boolean(app)) {
+      console.log('🏗 Creating app...');
+      const newApp = await caprover.appsRegister({ appName: APP_NAME });
+      console.log(newApp.description);
 
-    const { data } = await resLogin.json();
+      console.log('🔒 Enabling default subdomain SSL');
+      const sslEnabled = await caprover.appsEnableSSL({ appName: APP_NAME });
+      console.log(sslEnabled.description);
 
-    const resRegister = await fetch(registerAppEndPoint, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        'x-namespace': 'captain',
-        'x-captain-auth': data.token,
-      },
-      method: 'POST',
-      body: JSON.stringify({
-        appName: 'test',
-        hasPersistentData: false,
-      }),
-    });
+      if (sslEnabled.status > 102) {
+        process.exit(1);
+      }
+    }
 
-    const register = await resRegister.json();
+    if (Boolean(app) && !app.hasDefaultSubDomainSsl) {
+      console.log('🔒 Enabling default subdomain SSL');
+      const sslEnabled = await caprover.appsEnableSSL({ appName: APP_NAME });
+      console.log(sslEnabled.description);
+    }
 
-    console.log(`✨ ${register?.description}`);
+    console.log('🛠 Setting up the app');
+    const settings = await caprover.appsUpdate(DEFAULT_APP_CONFIG);
+    console.log(settings.description);
+
+    if (settings.status > 102) {
+      const deleted = await caprover.appsDelete({ appName: APP_NAME });
+      console.log(deleted.description);
+      process.exit(1);
+    }
+
     process.exit(0);
   } catch (err) {
     console.error(err);
@@ -46,4 +77,7 @@ async function main() {
   }
 }
 
-main();
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
---
+++ src/lib/caprover-api.js
@@ -0,0 +1,131 @@
+const fetch = require('node-fetch/lib/index');
+
+const defaultHeaders = {
+  accept: 'application/json',
+  'content-type': 'application/json;charset=UTF-8',
+  'x-namespace': 'captain',
+};
+
+class Caprover {
+  serverUrl;
+  token;
+  loginEndpoint;
+  appsEndpoint;
+  registerAppEndpoint;
+  updateAppEndpoint;
+  deleteAppEndpoint;
+  enableSSLEndpoint;
+
+  constructor(_serverUrl, _token) {
+    this.serverUrl = _serverUrl;
+    this.token = _token;
+    this.loginEndpoint = this.serverUrl + '/api/v2/login';
+    this.appsEndpoint = this.serverUrl + '/api/v2/user/apps/appDefinitions';
+    this.registerAppEndpoint = this.serverUrl + '/api/v2/user/apps/appDefinitions/register';
+    this.updateAppEndpoint = this.serverUrl + '/api/v2/user/apps/appDefinitions/update';
+    this.deleteAppEndpoint = this.serverUrl + '/api/v2/user/apps/appDefinitions/delete';
+    this.enableSSLEndpoint = this.serverUrl + '/api/v2/user/apps/appDefinitions/enablebasedomainssl';
+  }
+
+  static async init(_serverUrl, _password) {
+    const loginUrl = _serverUrl + '/api/v2/login';
+    const res = await fetch(loginUrl, {
+      headers: defaultHeaders,
+      method: 'POST',
+      body: JSON.stringify({ password: _password }),
+    });
+    const { data } = await res.json();
+    return new Caprover(_serverUrl, data.token);
+  }
+
+  async apps() {
+    const options = {
+      headers: { ...defaultHeaders, 'x-captain-auth': this.token },
+      method: 'GET',
+    };
+    const res = await fetch(this.appsEndpoint, options);
+    const apps = await res.json();
+    delete apps.data.defaultNginxConfig;
+    return apps;
+  }
+
+  async appsRegister({ appName, hasPersistentData = false }) {
+    const options = {
+      headers: { ...defaultHeaders, 'x-captain-auth': this.token },
+      method: 'POST',
+      body: JSON.stringify({
+        appName,
+        hasPersistentData,
+      }),
+    };
+    const res = await fetch(this.registerAppEndpoint, options);
+    return res.json();
+  }
+
+  async appsEnableSSL({ appName }) {
+    const options = {
+      headers: { ...defaultHeaders, 'x-captain-auth': this.token },
+      method: 'POST',
+      body: JSON.stringify({
+        appName,
+      }),
+    };
+    const res = await fetch(this.enableSSLEndpoint, options);
+    return res.json();
+  }
+
+  /**
+   *
+   * @param {*} body
+   * @returns
+   *
+   * {
+      appName:,
+      appDeployTokenConfig: {
+        enabled: true,
+      },
+      instanceCount: 1,
+      captainDefinitionRelativeFilePath: './captain-definition',
+      containerHttpPort: 3000,
+      notExposeAsWebApp: false,
+      forceSsl: true,
+      websocketSupport: false,
+      volumes: [],
+      ports: [],
+      description: '',
+      envVars: [
+        {
+          key: 'LINK_DOMAIN',
+          value: 'https://sniplink-preview-pr-11.mdecloud.tk',
+        },
+        {
+          key: 'DATABASE_URL',
+          value: 'mysql://user:passwd@@one-db.mdecloud.tk:6160/sniplink-dev',
+        },
+      ],
+    }
+   */
+  async appsUpdate(body) {
+    const options = {
+      headers: { ...defaultHeaders, 'x-captain-auth': this.token },
+      method: 'POST',
+      body: JSON.stringify(body),
+    };
+    const res = await fetch(this.updateAppEndpoint, options);
+    return res.json();
+  }
+
+  async appsDelete({ appName }) {
+    const options = {
+      headers: { ...defaultHeaders, 'x-captain-auth': this.token },
+      method: 'POST',
+      body: JSON.stringify({
+        appName,
+      }),
+    };
+    const res = await fetch(this.deleteAppEndpoint, options);
+    return res.json();
+  }
+}
+
+module.exports = Caprover;
